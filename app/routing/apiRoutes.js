c4c0853e9fdbb02e7a295b9607e2db8a27cc97c7
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-08-17 23:20:18 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,59 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+var friendData = require('../data/friends.js');
+var express = require("express");
+
+module.exports = function (app) {
+    app.get('/api/friends', (req, res) => {
+        res.json(friendData);
+    });
+
+    app.post("api/friends", (req, res) => {
+        var yourMatch = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+
+
+        var userResponses = req.body;
+        var userScores = userData.scores;
+        var userName = userData.name;
+        var userPic = userData.photo;
+        var totalDifference = 0;
+
+        // userResponses.forEach(current => {
+
+        // });
+
+        // Array.prototype.forEach = function( callback ){
+        //     var array = this;
+        //     for (var i = 0; i < array.length; i++){
+        //         callback( array[i] );
+        //     }
+        // }
+        for (var i = 0; i < friendData.length; i++) {
+            var current = userResponses[i];
+            console.log(friends[i].name);
+            console.log(friends[i].score);
+            totalDifference = 0;
+
+            for (var j = 0; j < 10; j++) {
+
+                if(totalDifference <= youMatch.difference) {
+
+                    yourMatch.name = friends[i].name;
+                    yourMatch.photo = friends[i].photo;
+                    yourMatch.difference = totalDifference;
+                };
+
+                friends.push(userData);
+
+                res.json(yourMatch);
+
+
+
+            };
+        };
+    });  
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,14 @@
-// A GET Route to /survey which should display the survey page.
-app.get("/survey", function(req,res){
-    res.json(survey);
-});
-// A default, catch-all route that leads to home.html which displays the home page.
-app.get("/", function(req,res){
-    res.json(home)
-});
+var express = require("express");
+var path = require("path");
+
+module.exports = function( app ){
+    // A default, catch-all route that leads to home.html which displays the home page.
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+    
+    // A GET Route to /survey which should display the survey page.
+    app.get('/survey', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+};
